1014ae726f80fc9e24e861fab4e30cc5265ec36b
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2022-10-12 16:40:43 +0200

    Oefening 2 deel 2 klaar

--- 10-java-challenges/src/main/java/challenges/Challenge_2.java
+++ 10-java-challenges/src/main/java/challenges/Challenge_2.java
@@ -3,9 +3,8 @@ package challenges;
 public class Challenge_2 {
 
 	/*
-	 * 2. Find the word Starting with an input string of words, find the
-	 * second-to-last word of the string. For example, an input of “I love
-	 * Codecademy” should return “love.”
+	 * Starting with an input string of words, find the second-to-last word of the
+	 * string. For example, an input of “I love Codecademy” should return “love.”
 	 * 
 	 * To make your program more challenging, allow for a second numerical input, n,
 	 * that results in returning the nth word of a string. So, for the string “I can
@@ -35,15 +34,15 @@ public class Challenge_2 {
 		}
 		return string;
 	}
-	
+
 	////////////////////////////////////////////////// PART 2
-	
+
 	private static String findWordAtIndex(String string, int wordAtIndex) {
 		String[] words = string.split(" ");
 		StringBuilder sb2 = new StringBuilder();
-		
-		for(int i = -1; i <= words.length; i++) {
-			if(i == wordAtIndex - 1) {
+
+		for (int i = -1; i <= words.length; i++) {
+			if (i == wordAtIndex - 1) {
 				return sb2.append(words[i]).toString();
 			}
 		}
