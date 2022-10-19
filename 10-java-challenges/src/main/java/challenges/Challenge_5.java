2fcd83b2ad352a74ee75350b80878eb27997b758
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2022-10-19 16:32:02 +0200

    Challenge 5 deel 2 klaar

--- 10-java-challenges/src/main/java/challenges/Challenge_5.java
+++ 10-java-challenges/src/main/java/challenges/Challenge_5.java
@@ -1,5 +1,10 @@
 package challenges;
 
+import java.nio.file.DirectoryStream.Filter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Challenge_5 {
 
 	/*
@@ -15,19 +20,21 @@ public class Challenge_5 {
 
 	public static void main(String[] args) {
 
-		String sentence = "The quick brown fox jumps over the lazy dog.";
+		String sentence = "The quick Brown fox jumps over the lazy dog.";
+		String sentence2 = "Mr. Jock, TV quiz Ph.D., bags few lynx.";
 
-		System.out.println("Sentence is pangram: " + checkIfPangram(sentence));
+
+		System.out.println("Sentence is a pangram: " + checkIfPangram(sentence));
+		System.out.println("Sentence is a perfect pangram: " + checkIfPerfectPangram(sentence2));
 	}
 
 	private static boolean checkIfPangram(String sentence) {
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		String[] alphabetArray = alphabet.split("|");
+		String[] alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("|");
+		sentence = sentence.toLowerCase();
 
 		if (sentence.length() < alphabetArray.length) {
 			return false;
 		} else {
-
 			for (int i = 0; i <= (alphabetArray.length - 1); i++) {
 				if (!sentence.contains(alphabetArray[i])) {
 					System.out.println("letter " + alphabetArray[i] + " not found in the sentence.");
@@ -36,5 +43,42 @@ public class Challenge_5 {
 			}
 		}
 		return true;
+	}
+
+	////////////////////////////////////////////////////// PART 2
+
+	private static boolean checkIfPerfectPangram(String sentence) {
+		String[] alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("|");
+
+		if (checkIfPangram(sentence)) {
+			String[] sentenceArray = sentence.toLowerCase().split("|");
+			Arrays.sort(sentenceArray);
+
+			for (int i = 0; i <= (alphabetArray.length - 1); i++) {
+				if (!sentenceArray[0].equals("a")) {
+					Arrays.sort(sentenceArray);
+					sentenceArray = removeFromArray(sentenceArray, 0).toArray(new String[0]);
+					if (Arrays.equals(alphabetArray, sentenceArray)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public static List<String> removeFromArray(String[] stringArray, int index) {
+		List<String> list = new ArrayList<>(Arrays.asList(stringArray));
+
+		try {
+			list.remove(index);
+			list.remove(null);
+
+			return list;
+
+		} catch (IndexOutOfBoundsException e) {
+			e.getStackTrace();
+		}
+		return list;
 	}
 }
