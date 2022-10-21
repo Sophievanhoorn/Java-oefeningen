ce950a68be873c684455022f8040739748bdd077
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2022-10-21 10:35:12 +0200

    Refactoring van deel 1

--- 10-java-challenges/src/main/java/challenges/Challenge_4.java
+++ 10-java-challenges/src/main/java/challenges/Challenge_4.java
@@ -1,6 +1,8 @@
 package challenges;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Challenge_4 {
 
@@ -19,42 +21,27 @@ public class Challenge_4 {
 
 	public static void main(String[] args) {
 
-		String[] inputArray = { "pairs", "Paris"};
+		String[] inputArray = { "pairs", "Paris", "prais" };
 		String[] wordArray = { "tar", "rat", "art", "meats", "steam" };
 
-		System.out.println("Words are anograms = " + checkIfAnagrams(inputArray));
+//		System.out.println("Words are anagrams = " + checkIfAnagrams(inputArray));
 		System.out.println(sortAnagramArray(wordArray));
 	}
 
 	public static boolean checkIfAnagrams(String[] inputArray) {
 
 		boolean isAnagram = false;
-		int count = 0;
-		int i = 0;
+		int x = 1;
 
-		for (String input : inputArray) {
-			input = inputArray[i].toLowerCase();
+		for (int i = 0; x != inputArray.length; i++) {
+			if (x != inputArray.length) {
+				try {
+					isAnagram = compareWordInput(inputArray[i], inputArray[x]);
+					x++;
 
-			try {
-				String nextInput = inputArray[i + 1].toLowerCase();
-				if (input.length() != nextInput.length()) {
-					return false;
-				}else {
-				i++;
-				if (count == inputArray.length) {
-					return isAnagram;
-				} else {
-						char[] firstInputToArray = input.toCharArray();
-						char[] nextInputToArray = nextInput.toCharArray();
-						Arrays.sort(firstInputToArray);
-						Arrays.sort(nextInputToArray);
-
-						isAnagram = Arrays.equals(firstInputToArray, nextInputToArray);
-						count++;
-					}
+				} catch (IndexOutOfBoundsException e) {
+					System.err.println(e.getMessage());
 				}
-			} catch (IndexOutOfBoundsException e) {
-				e.getStackTrace();
 			}
 		}
 		return isAnagram;
@@ -62,15 +49,42 @@ public class Challenge_4 {
 
 	///////////////////////////////////////// PART 2
 
-	public static String[] sortAnagramArray(String[] inputArray) {
+	public static String[] sortAnagramArray(String[] wordArray) {
+		String[] sortedAnagramArray = null;
+		int i = 0;
+		int x = 1;
 
-		for (int i = 0; i < inputArray.length; i++) {
-			
-			checkIfAnagrams(inputArray);
-			String input = inputArray[i].toLowerCase();	
+		for (i = 0; i < wordArray.length; i++) {
 
+			while (compareWordInput(wordArray[i], wordArray[x])) {
+				List<String> list = new ArrayList<>();
+				list.add(wordArray[i]);
+				list.add(wordArray[x]);
+			}
+			List[] listArray = null;
 		}
 
-		return inputArray;
+		return sortedAnagramArray;
+	}
+
+	public static boolean compareWordInput(String input, String nextInput) {
+
+		input = input.toLowerCase();
+		System.out.println(input);
+
+		nextInput = nextInput.toLowerCase();
+		System.out.println(nextInput);
+
+		if (input.length() == nextInput.length()) {
+
+			char[] firstInputCharArray = input.toCharArray();
+			char[] nextInputCharArray = nextInput.toCharArray();
+
+			Arrays.sort(firstInputCharArray);
+			Arrays.sort(nextInputCharArray);
+
+			return Arrays.equals(firstInputCharArray, nextInputCharArray);
+		}
+		return false;
 	}
 }
