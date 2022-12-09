5d103afdba3c66136856ca9718b2e313e1093447
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2022-12-09 16:27:23 +0100

    Challenge 8 deel 1 & 2 klaar

--- 10-java-challenges/src/main/java/challenges/Challenge_8.java
+++ 10-java-challenges/src/main/java/challenges/Challenge_8.java
@@ -1,7 +1,6 @@
 package challenges;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Challenge_8 {
@@ -17,38 +16,60 @@ public class Challenge_8 {
 
 	public static void main(String[] args) {
 
-		int[] numberArray = { 1, 14, 7, 5, 20, 20 };
-		int[] numberArray2 = { 1, 14 };
-		int[] numberArray3 = {};
+		int[] numberArray = { 1, 34, 7, 5, 20, 20 };
+		int[] numberArray2 = { 1, 14, 22 };
+		int[] numberArray3 = { 2 };
 
 		System.out.println(getMaxProduct(numberArray));
 		System.out.println(getMaxProduct(numberArray2));
 		System.out.println(getMaxProduct(numberArray3));
+
+		System.out.println(getMaxProductFrom2Arrays(numberArray, numberArray2));
 	}
 
 	public static String getMaxProduct(int[] numberArray) {
-		int numMin = 0;
-		int numMax = 0;
-		int arrayLength = numberArray.length;
 
-		if (arrayLength >= 2) {
-			Arrays.sort(numberArray);
+		if (numberArray.length >= 2) {
 
-			numMin = Array.getInt(numberArray, arrayLength - 2);
-			numMax = Array.getInt(numberArray, arrayLength - 1);
-
-			int result = numMin * numMax;
-			int[] resultArray = { numMin, numMax, result };
+			int[] resultArray = { getMinNum(numberArray), getMaxNum(numberArray),
+					(getMinNum(numberArray) * getMaxNum(numberArray)) };
 
 			return Arrays.toString(resultArray);
 
 		} else {
-			System.out.println("The input array contains " + arrayLength
-					+ " number(s). This method needs at least 2 numbers to work.");
-			return "";
+			return "The input array contains " + numberArray.length
+					+ " number(s). This method needs at least 2 numbers to work.";
 		}
 	}
 
 	//////////////////////////////////////////////////// PART 2
 
+	public static String getMaxProductFrom2Arrays(int[] numberArray, int[] numberArray2) {
+
+		if (numberArray.length >= 2 && numberArray2.length >= 2) {
+
+			int maxNumArray1 = getMaxNum(numberArray);
+			int maxNumArray2 = getMaxNum(numberArray2);
+
+			int[] resultArray = { maxNumArray1, maxNumArray2, (maxNumArray1 * maxNumArray2) };
+
+			return Arrays.toString(resultArray);
+		} else {
+			System.out.println("The input array contains " + numberArray2.length
+					+ " number(s). This method needs at least 2 numbers to work.");
+			return "";
+		}
+	}
+
+	public static int getMaxNum(int[] numberArray) {
+		Arrays.sort(numberArray);
+
+		return Array.getInt(numberArray, numberArray.length - 1);
+	}
+
+	public static int getMinNum(int[] numberArray) {
+		Arrays.sort(numberArray);
+
+		return Array.getInt(numberArray, numberArray.length - 2);
+	}
 }
