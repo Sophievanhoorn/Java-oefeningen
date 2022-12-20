fec0e675ab78eb1add2d910a58aa3e00e51023d2
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2022-12-20 13:11:47 +0100

    Palindrome_Challenge deel 1 & 2 klaar

--- 10-java-challenges/src/main/java/challenges/Palindrome_Challenge.java
+++ 10-java-challenges/src/main/java/challenges/Palindrome_Challenge.java
@@ -1,5 +1,6 @@
 package challenges;
 
+import java.util.Arrays;
 
 public class Palindrome_Challenge {
 
@@ -14,14 +15,36 @@ public class Palindrome_Challenge {
 
 	public static void main(String[] args) {
 
-		String sentence = "De mooie zeeman nam Anna mee zei oom Ed.";
-		String sentence2 = "Kauwen paarden als kraaien; dat is waar, niet waar is dat kraaien als paarden kauwen.";
+		String sentence = "De mooie zeeman nam Anna mee zei oom Eda.";
+		String sentence2 = "Kauwen paarden als kraaien; dat is waar, niet waar yay is dat kraaien als paarden kauwen.";
 
-		isPalindrome(sentence);
+//		isPalindrome(sentence);
+		isPalindromeSentence(sentence2);
 
 	}
 
-	public static boolean isPalindrome(String sentence) {
+	public static void isPalindrome(String sentence) {
+
+		char[] strippedSentence = stripWholeSentence(sentence).toCharArray();
+
+		boolean check = false;
+
+		for (int i = 0, j = strippedSentence.length - 1; i < strippedSentence.length - 1 && j >= 0; i++, j--) {
+			if (strippedSentence[i] != strippedSentence[j]) {
+				strippedSentence[i] = Character.toUpperCase(strippedSentence[i]);
+				strippedSentence[j] = Character.toUpperCase(strippedSentence[j]);
+
+				System.out.printf(String.format("The sentence is not a Palindrome sentence : %s", String.copyValueOf(strippedSentence)));
+				break;
+			} else {
+				check = true;
+			}
+		}
+		if (check)
+			System.out.println("The sentence '" + sentence + "' is a Palindrome!");
+	}
+
+	public static String stripWholeSentence(String sentence) {
 
 		char[] array = sentence.toLowerCase().toCharArray();
 		StringBuilder sb = new StringBuilder();
@@ -31,27 +54,41 @@ public class Palindrome_Challenge {
 				sb.append(array[i]);
 			}
 		}
-		boolean check = false;
-
-		char[] strippedSentence = sb.toString().toCharArray();
-
-		for (int x = 0, y = strippedSentence.length - 1; x < strippedSentence.length - 1 && y >= 0; x++, y--) {
-			if (strippedSentence[x] != strippedSentence[y]) {
-				char letter1 = strippedSentence[x];
-				char letter2 = strippedSentence[y];
-				System.out.println("The sentence is not a Palindrome. Letter " + letter1 + " at index " + x
-						+ " and letter " + letter2 + " at index " + "" + y + " are not the same.");
-				break;
-			} else {
-				check = true;
-			}
-		}
-		if (check)
-			System.out.println("The sentence " + sentence + " is a Palindrome!");
-
-		return false;
+		return sb.toString();
 	}
-	
+
 	///////////////////////////////////////////////////////////////////// DEEL 2
 
+	public static void isPalindromeSentence(String sentence) {
+
+		boolean check = false;
+
+		String[] strippedSentence = stripSentenceByWords(sentence.toLowerCase()).split(" ");
+
+		for (int i = 0, j = strippedSentence.length - 1; i < strippedSentence.length - 1 && j >= 0; i++, j--) {
+			if (!strippedSentence[i].equals(strippedSentence[j])) {
+				strippedSentence[i] = strippedSentence[i].toUpperCase();
+				strippedSentence[j] = strippedSentence[j].toUpperCase();
+				System.out.println(String.format("The sentence is not a Palindrome sentence : %s",
+						Arrays.toString(strippedSentence)));
+				break;
+			} else
+				check = true;
+		}
+		if (check)
+			System.out.println(String.format("The sentence : %s is a Palandrome sentence!", sentence));
+	}
+
+	public static String stripSentenceByWords(String sentence) {
+
+		char[] array = sentence.toLowerCase().toCharArray();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < array.length; i++) {
+			if (Character.isAlphabetic(array[i]) || Character.isWhitespace(array[i])) {
+				sb.append(array[i]);
+			}
+		}
+		return sb.toString();
+	}
 }
