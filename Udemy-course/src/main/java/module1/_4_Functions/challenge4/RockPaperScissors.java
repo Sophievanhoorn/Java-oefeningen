c5dd0ce57030c8f0da103716b1055dea165ab54d
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2023-01-11 13:25:11 +0100

    Udemy course - Rock Paper Scissors challenge

--- Udemy-course/src/main/java/module1/_4_Functions/challenge4/RockPaperScissors.java
+++ Udemy-course/src/main/java/module1/_4_Functions/challenge4/RockPaperScissors.java
@@ -3,102 +3,104 @@ package module1._4_Functions.challenge4;
 import java.util.Scanner;
 
 public class RockPaperScissors {
-    public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
 
-        System.out.println("Let's play Rock Paper Scissors.");
-        System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.\n");
-        System.out.println("Are you ready? Write 'yes' if you are.");
+		Scanner scan = new Scanner(System.in);
 
-    //Task 1: See if the user wants to play. 
+		System.out.println("Let's play Rock Paper Scissors.");
+		System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.\n");
+		System.out.println("Are you ready? Write 'yes' if you are.");
 
-    /*Task 2: Set up the game
-    
+		// Task 1: See if the user wants to play.
+		if (scan.nextLine().equalsIgnoreCase("yes")) {
+			System.out.println("\nGreat!");
+			playGame();
+		} else {
+			System.out.println("\nAlright, see you next time!");
+			System.exit(0);
+		}
 
-       • if the answer is yes: 
-             – print: Great!
-             – print: rock - paper - scissors, shoot!
-             – pick up user's choice.
-             – get the computer choice (can only be done after task 3).
-             – get the result (can only be done after task 4)
-             – print everything (can only be done after task 5).
+		scan.close();
+	}
 
-       • else:
-             – print: Darn, some other time...!        
-    */
-              
+	public static void playGame() {
+		Scanner scan = new Scanner(System.in);
 
-        scan.close();
-    }
+		System.out.println("rock - paper - scissors - shoot");
+		String userAnswer = scan.nextLine();
+		String cpuAnswer = getChoice();
+		String result = result(userAnswer, cpuAnswer);
 
+		System.out.println("\n" + printResult(userAnswer, cpuAnswer, result));
 
-    //Task 3  – Write a function where the computer picks a random choice.
+		System.out.println("\nWould you like to play again? yes/no");
 
-    /**
-     * Function name: computerChoice - picks randomly between rock paper and scissors
-     * @return a choice (String).
-     * 
-     * Inside the function:
-     *   1. Picks a random number between 0 and 2.
-     *   2. if 0: returns the choice 'rock'
-     *      if 1: returns the choice 'paper'
-     *      if 2: returns the choice 'scissors'
-     */
+		if (scan.nextLine().equalsIgnoreCase("yes")) {
+			System.out.println("\nAlright, here we go!");
+			playGame();
 
+		} else {
+			System.out.println("\nAlright, see you next time!");
+			System.exit(0);
+		}
+		scan.close();
+	}
 
+	// Task 3 – Write a function where the computer picks a random choice.
 
+	public static String getChoice() {
+		String choice = "";
 
-    //Task 4  – Write a function that compares the choices and returns the result.
+		switch ((int) (Math.random() * 3) + 1) {
+		case 1:
+			choice = "rock";
+			break;
+		case 2:
+			choice = "paper";
+			break;
+		case 3:
+			choice = "scissors";
+			break;
+		default:
+			System.out.println("Something went wrong. The game will close");
+			System.exit(0);
+		}
 
-    /**
-     * Function name: result - It returns the result of the game.
-     *  @param yourChoice (String)
-     *  @param computerChoice (String) 
-     *  @return result (String) 
+		return choice;
+	}
 
-     * Inside the function:
-     *   
-     *   1. result is "You win!" if:
-     *
-     *       You: "rock"      Computer: "scissors"
-     *       You: "paper"     Computer: "rock"
-     *       You: "scissors"  Computer: "paper"
-     *
-     *   2. result is "You lose" if:
-     * 
-     *       Computer: "rock"      You: "scissors"
-     *       Computer: "paper"     You: "rock"
-     *       Computer: "scissors"  You: "paper"
-     *
-     *   3. Otherwise, the result is "You lose!":
-     * 
-     * 
-     *   4. Otherwise, print "INVALID CHOICE" and exit the program.
-     *
-     *
-     */
+//	rock > paper > stone > rock
+	// Task 4 – Write a function that compares the choices and returns the result.
 
-    public static String result(String yourChoice, String computerChoice) {
-        String result = "";
+	public static String result(String userAnswer, String cpuAnswer) {
 
-        return result;
-      }
- 
-     //Task 5  – Write a function that prints your choice, the computer's, and the result.
+		String user = userAnswer.toLowerCase();
+		String cpu = cpuAnswer.toLowerCase();
 
-    /**
-     * Name: printResult - It prints everything (your choice, computer choice, result)
-     * @param yourChoice (String)
-     * @param computerChoice (String)
-     * @param result (String)
-     * 
-     * Inside the function:
-     * 
-     *  1. prints everything:
-     *      – prints: You chose:          <your choice>
-     *      – prints: The computer chose: <computer choice>
-     *      – prints: <result>
-     */
+		String result = "";
 
+		if (user.equals("rock") && cpu.equals("scissors") || user.equals("scissors") && cpu.equals("paper")
+				|| user.equals("paper") && cpu.equals("rock")) {
+			result = user + " beats " + cpu + ", You win!";
+
+		} else if (cpu.equals("rock") && user.equals("scissors") || cpu.equals("scissors") && user.equals("paper")
+				|| cpu.equals("paper") && user.equals("rock")) {
+			result = cpu + " beats " + user + ", You lose...";
+
+		} else if (user.equalsIgnoreCase(cpu)) {
+			result = "It's a tie!";
+		} else {
+			result = "It seems like something went wrong... The game will now close.";
+			System.exit(0);
+		}
+
+		return result;
+	}
+
+// Task 5 – Write a function that prints your choice, the computer's, and the
+// result.
+
+	public static String printResult(String userAnswer, String cpuAnswer, String result) {
+		return "You: " + userAnswer + "\ncpu: " + cpuAnswer + "\n" + result;
+	}
 }
