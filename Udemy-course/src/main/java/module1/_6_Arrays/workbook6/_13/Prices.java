e10bd8402fe9f0558139464e3b71375f3f4d319a
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2023-03-03 10:46:18 +0100

    Workbook 6.13 & 6.14

--- Udemy-course/src/main/java/module1/_6_Arrays/workbook6/_13/Prices.java
+++ Udemy-course/src/main/java/module1/_6_Arrays/workbook6/_13/Prices.java
@@ -1,7 +1,36 @@
 package module1._6_Arrays.workbook6._13;
 
 public class Prices {
-    public static void main(String[] args) {
-        // The instructions for this workbook are on Learn the Part (workbook 6.13)
-    }
+	public static void main(String[] args) {
+		// The instructions for this workbook are on Learn the Part (workbook 6.13)
+
+		// Create a 2D array of double values that can hold 3 rows and 5 elements per
+		// row.
+		double[][] prices = { { 12.99, 8.99, 9.99, 10.49, 11.99 }, { 0.99, 1.99, 2.49, 1.49, 2.99 },
+				{ 8.99, 7.99, 9.49, 9.99, 10.99 }, };
+
+		for (int i = 0; i < prices.length; i++) {
+			
+			switch (i) {
+			case 0:
+				System.out.print("Baking: ");
+				for (int j = 0; j < prices[i].length; j++) {
+					System.out.print(prices[i][j] + " ");
+				}
+				break;
+			case 1:
+				System.out.print("\nBeverage: ");
+				for (int j = 0; j < prices[i].length; j++) {
+					System.out.print(prices[i][j] + " ");
+				}
+				break;
+			case 2:
+				System.out.print("\nCereals: ");
+				for (int j = 0; j < prices[i].length; j++) {
+					System.out.print(prices[i][j] + " ");
+				}
+				break;
+			}
+		}
+	}
 }
--- Udemy-course/src/main/java/module1/_6_Arrays/workbook6/_14/RandomNumbers.java
+++ Udemy-course/src/main/java/module1/_6_Arrays/workbook6/_14/RandomNumbers.java
@@ -1,9 +1,47 @@
 package module1._6_Arrays.workbook6._14;
 
-public class RandomNumbers {
-    public static void main(String[] args) {
-        
-        // The instructions for this workbook are on Learn the Part (Workbook 6.14)
-    }
+import java.util.Arrays;
 
+public class RandomNumbers {
+	public static void main(String[] args) {
+
+		// The instructions for this workbook are on Learn the Part (Workbook 6.14)
+
+		int[][] array = new int[100][10];
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = randomNumber();
+			}
+		}
+
+		print2DArray(array);
+	}
+
+	/**
+	 * function name: randomNumbers (int)
+	 * 
+	 * @return (int)
+	 * 
+	 *         What it does: Generates a random number
+	 */
+	public static int randomNumber() {
+		return (int) (Math.random() * 100);
+	}
+
+	/**
+	 * function name: print2DArray
+	 * 
+	 * @param array (int[][])
+	 * 
+	 *              What it does: Prints the 2D array
+	 */
+	public static void print2DArray(int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
 }
