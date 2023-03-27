0253af86ebaf166185ee644b30c46090be8a5d84
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2023-03-27 16:55:08 +0200

    Udemy Challenge Javapedia klaar

--- Udemy-course/src/main/java/module1/_6_Arrays/Javapedia/Javapedia.java
+++ Udemy-course/src/main/java/module1/_6_Arrays/Javapedia/Javapedia.java
@@ -1,62 +1,74 @@
 package module1._6_Arrays.Javapedia;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Javapedia {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("\n**********Javapedia**********");
-        System.out.println("How many historical figures will you register?");
-        //Task 1 – Ask the user: how many historical figures will you register?
-        //       – Store the value.
-        
-        //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
-         
-        //Watch out for the nextLine() pitfall. 
-        /* Task 3 
-        for (____) {
+	public static void main(String[] args) {
+		System.out.println("\n**********Javapedia**********");
 
-            System.out.println("\n\tFigure " + (i+1)); 
+		Scanner scan = new Scanner(System.in);
+		int numberHistoricalFigures = 0;
 
-            System.out.print("\t - Name: ");
-            pick up and store figure's name.   
+		while (true) {
+			try {
+				System.out.println("How many historical figures will you register?");
+				numberHistoricalFigures = scan.nextInt();
+				scan.nextLine();
+				if (numberHistoricalFigures > 0) {
+					break;
+				} else {
+					System.out.println("Input must be a number > 0");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Input must be a number > 0");
+				scan.next();
+			}
+		}
 
-            System.out.print("\t - Date of birth: ");
-            pick up and store figure's birthday.
+		String[][] array = new String[numberHistoricalFigures][3];
 
-            System.out.print("\t - Occupation: ");
-            pick up and store figure's occupation. 
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("\n\tFigure " + (i + 1));
+			System.out.print("\t - Name: ");
+			// Name
+			array[i][0] = scan.nextLine();
 
-            System.out.print("\n");
+			System.out.print("\t - Date of birth: ");
+			// Birthday
+			array[i][1] = scan.nextLine();
 
-        }
-        
-        */
-        System.out.println("These are the values you stored:"); 
-        //Task 4: call print2DArray. 
+			// Occupasion
+			System.out.print("\t - Occupation: ");
+			array[i][2] = scan.nextLine();
+		}
 
-        System.out.print("\nWho do you want information on? ");  
-        
-        /*Task 5: Let the user search the database by name. 
-            If there's a match:
-              print(    tab of space    Name: <name>)
-              print(    tab of space    Date of birth: <date of birth>)
-              print(    tab of space    Occupation: <occupation>)
+		System.out.println("\nThese are the values you stored:");
+		print2DArray(array);
 
-        */        
+		System.out.print("\n\nWho do you want information on?\n");
+		getName(scan.nextLine(), array);
 
-        scan.close();
-    }
+		scan.close();
+	}
 
-    /**
-     * Function name: print2DArray
-     * @param array (String[][])
-     * 
-     * Inside the function
-     *  1. print the database
-     *     • a tab of space precedes each row.
-     *     • each value in database has one space from the other value. 
-     *     • print a new line.
-     */
+	public static void getName(String name, String[][] array) {
+		int hits = 0;
 
+		for (int i = 0; i < array.length; i++) {
+			if (array[i][0].equalsIgnoreCase(name)) {
+				System.out.println("\tName: " + array[i][0] + "\n\tDate of birth: " + array[i][1] + "\n\tOccupation: "
+						+ array[i][2]);
+				hits++;
+			}
+		}
+		System.out.println("\nNumber of results: " + hits);
+	}
+
+	public static void print2DArray(String[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print("\n\t - " + Arrays.toString(array[i]));
+		}
+	}
 }
