62c020688b5ae5d7c08ddb92c1c9f73ff39028cb
sophievanhoorn <sophie.van.hoorn@novadoc.nl>
2023-03-30 14:25:45 +0200

    Udemy course Challenge 6 klaar

--- Udemy-course/src/main/java/module1/_6_Arrays/challenge6/TicTacToe.java
+++ Udemy-course/src/main/java/module1/_6_Arrays/challenge6/TicTacToe.java
@@ -4,78 +4,156 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        
-            System.out.println("\nLet's play tic tac toe");
+	static Scanner scan = new Scanner(System.in);
 
-            //Task 1: Create an array with three rows of '_' characters.
+	public static void main(String[] args) {
 
-            //Task 2: Call the function printBoard();
+		System.out.println("\nLet's play tic tac toe");
 
-             
-              /*
-              {  Task 3: Loop through turns.
+		char[][] board = createBoard();
 
-                  if (X) turn {
-                     Task 4: call askUser(). 
-                     Task 5: populate the board using askUser's return value.
-                  } else {
-                      Task 4: call askUser(). 
-                      Task 5: populate the board using askUser's return value. Then, print it.
+		printBoard(board);
+		char turn = 'X';
 
-                  }
+		// Loop through turns starting with X
+		for (int i = 1; i <= 9; i++) {
+			System.out.println("Turn: " + i);
 
-                Task 6 - Call the function.
-                   if return value == 3 {
-                     print: X wins and break the loop
-                  } else if return value == -3 {
-                     print: O wins and break the loop
-                  }
+			if (turn == 'X') {
+				System.out.println("Player: X");
+				int[] userX = askUser(board);
+				board[userX[0]][userX[1]] = 'X';
+				turn = 'O';
 
-              } 
-              */
+				printBoard(board);
+				checkWin(board);
 
-            scan.close();
-        }
+			} else {
+				System.out.println("Player: O");
+				int[] userO = askUser(board);
+				board[userO[0]][userO[1]] = 'O';
+				turn = 'X';
 
+				printBoard(board);
+				checkWin(board);
+			}
+			if (i == 8) {
+				System.out.println("It's a tie!");
+			}
+		}
+	}
 
-    /** Task 2 - Write a function that prints the board.
-     * Function name - printBoard()
-     * @param board (char[][])
-     * 
-     * Inside the function:
-     *   1. print a new line.
-     *   2. print the board.
-     *      • separate each row by two lines. 
-     *      • each row precedes a tab of space
-     *      • each character in the grid has one space from the other character
-     */        
+	public static void printBoard(char[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(Character.toString(array[i][0]) + "\t" + Character.toString(array[i][1]) + "\t"
+					+ Character.toString(array[i][2]) + "\n\n");
+		}
+	}
 
-   /** Task 4 - Write a function that lets the user choose a spot
-     * Function name – askUser
-     * @param board (char[][] board)
-     * @return spot (int[])
-     * 
-     * Inside the function
-     *   1. Asks the user: - pick a row and column number: 
-     *   2. Check if the spot is taken. If so, let the user choose again.
-     *   3. Return the row and column in an int[] array.
-     * 
-     */
+	public static char[][] createBoard() {
+		char[][] board = new char[3][3];
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j] = '_';
+			}
+		}
+		return board;
+	}
 
-    /** Task 6 - Write a function that determines the winner
-     * Function name - checkWin 
-     * @param board (char[][])
-     * @return count (int)
-     * 
-     * Inside the function:
-     *   1. Make a count variable that starts at 0.
-     *   2. Check every row for a straight X or straight O (Task 7).
-     *   3. Check every column for a straight X or straight O (Task 8).
-     *   4. Check the left diagonal for a straight X or straight O (Task 9).
-     *   5. Check the right diagonal for a straight X or straight O (Task 10).
-     */
+	public static int[] askUser(char[][] board) {
+		int row = 0;
+		int column = 0;
+		int[] answer = { row, column };
+		boolean emptySpot = false;
 
+		while (!emptySpot) {
+			System.out.println("pick a row and column number");
+			System.out.print("\tRow: ");
+			row = scan.nextInt() - 1;
+			scan.nextLine();
 
+			System.out.print("\tColumn: ");
+			column = scan.nextInt() - 1;
+			scan.nextLine();
+
+			if (Character.compare(board[row][column], '_') == 0) {
+				answer[0] = row;
+				answer[1] = column;
+				emptySpot = true;
+			} else {
+				System.out.println("This spot is taken, please choose again");
+			}
+		}
+		return answer;
+	}
+
+	public static int checkWin(char[][] board) {
+
+		if (horizontalCheck(board) == 3 || verticalCheck(board) == 3 || diagonalCheck(board) == 3) {
+			System.out.println("Player X is the winner!");
+			System.exit(0);
+
+		} else if (horizontalCheck(board) == -3 || verticalCheck(board) == -3 || diagonalCheck(board) == -3) {
+			System.out.println("Player O is the winner!");
+			System.exit(0);
+		}
+		return 0;
+	}
+
+	public static int horizontalCheck(char[][] board) {
+		for (int i = 0; i < board[0].length; i++) {
+			int count = 0;
+			for (int j = 0; j < board.length; j++)
+				if (Character.compare(board[i][j], 'X') == 0) {
+					count++;
+				} else if (Character.compare(board[i][j], 'O') == 0) {
+					count--;
+				}
+			if (count == 3) {
+				return count;
+			} else if (count == -3) {
+				return count;
+			}
+		}
+		return 0;
+	}
+
+	public static int verticalCheck(char[][] board) {
+		// Horizontal
+		for (int i = 0; i < board[0].length; i++) {
+			int count = 0;
+			for (int j = 0; j < board.length; j++)
+				if (Character.compare(board[j][i], 'X') == 0) {
+					count++;
+				} else if (Character.compare(board[j][i], 'O') == 0) {
+					count--;
+				}
+			if (count == 3) {
+				return count;
+			} else if (count == -3) {
+				return count;
+			}
+		}
+		return 0;
+	}
+
+	public static int diagonalCheck(char[][] board) {
+		// Horizontal
+		for (int a = 0; a < 2; a++) {
+			int count = 0;
+			for (int i = 0, j = 2; i < board[0].length; i++, j--) {
+				if (Character.compare(board[i][i], 'X') == 0 || Character.compare(board[j][i], 'X') == 0) {
+					count++;
+				} else if (Character.compare(board[i][i], 'O') == 0 || Character.compare(board[j][i], 'O') == 0) {
+					count--;
+				}
+				if (count == 3) {
+					return count;
+				} else if (count == -3) {
+					return count;
+				}
+			}
+		}
+		return 0;
+	}
 }
